8c754c0c4831bd7cab53b7e79427d005e13efefc
propotolki <propotolki52@gmail.com>
2025-08-19 23:39:48 +0300

    23

---
+++ docs/assets/Div-DQ82myNL.js
@@ -0,0 +1,1 @@
+import{l as o,R as a,q as t}from"./index-jcpojfjV.js";const i=s=>o.jsx(a,t({baseClassName:"vkuiDiv"},s));export{i as D};
--- docs/assets/Div-DlVZe4XB.js
+++
@@ -1,1 +0,0 @@
-import{l as o,R as a,q as t}from"./index-Bao5TLqz.js";const i=s=>o.jsx(a,t({baseClassName:"vkuiDiv"},s));export{i as D};
